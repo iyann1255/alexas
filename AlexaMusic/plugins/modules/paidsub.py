1ddecca387ebf613940eb2cd4b0d23961f259ce8
iyann1255 <tessbarca1981@gmail.com>
2025-06-28 03:31:46 +0700

    Update paidsub.py

--- AlexaMusic/plugins/modules/paidsub.py
+++ AlexaMusic/plugins/modules/paidsub.py
@@ -10,3 +10,38 @@ from AlexaMusic.utils.database import get_served_users, get_served_chats
 
 
 OWNER_ID = 7931457261
+
+def subcribe(func):
+    async def wrapper(_, message: Message):
+        user_id = message.from_user.id
+        user_name = message.from_user.first_name
+        rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+        if not JOIN:  # Not compulsory
+            return
+        try:
+            try:
+                await app.get_chat_member(JOIN, message.from_user.id)
+            except UserNotParticipant:
+                if JOIN.isalpha():
+                    link = "https://t.me/" + JOIN
+                else:
+                    chat_info = await app.get_chat(JOIN)
+                    chat_info.invite_link
+                try:
+                    await message.reply(
+                        f"ʜᴇʏ {rpk}. ᴀɢᴀʀ ʙɪsᴀ ᴍᴇᴍᴜᴛᴀʀ ᴍᴜsɪᴋ, ᴀɴᴅᴀ ʜᴀʀᴜs ᴊᴏɪɴ ᴋᴇ ɢʀᴜᴘ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ . ᴋᴀʟᴀᴜ ᴀᴅᴀ ᴍᴀꜱᴀʟᴀʜ ᴘᴄ @awdfilm",
+                        disable_web_page_preview=True,
+                        reply_markup=InlineKeyboardMarkup(
+                            [[InlineKeyboardButton("🗣️ GABUNG SINI 😅", url=link)]]
+                        ),
+                    )
+                    await message.stop_propagation()
+                except ChatWriteForbidden:
+                    pass
+        except ChatAdminRequired:
+            await message.reply(
+                f"Saya bukan admin di chat : {JOIN} !"
+            )
+        return await func(_, message)
+
+    return wrapper
