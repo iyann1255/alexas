48e99c7633fe424b2ffb41c0880278b5f4537640
iyann1255 <tessbarca1981@gmail.com>
2025-06-28 03:45:06 +0700

    Update paidsub.py

--- AlexaMusic/plugins/modules/paidsub.py
+++ AlexaMusic/plugins/modules/paidsub.py
@@ -1,18 +1,44 @@
-from pyrogram.types import Message
-from functools import wraps
+import asyncio
+from AlexaMusic import app
+from pyrogram import Client, filters
+from datetime import datetime, timedelta
+from pyrogram.errors import FloodWait
+from AlexaMusic.core.mongo import db as alexa
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from AlexaMusic.utils.database import get_served_users, get_served_chats
 
-def paid_subscriber_required(func):
-    @wraps(func)
+
+def subcribe(func):
     async def wrapper(_, message: Message):
         user_id = message.from_user.id
-        # Misal: cek di database atau API apakah user berlangganan
-        if is_paid_subscriber(user_id):
-            return await func(_, message)
-        else:
-            await message.reply("Fitur ini hanya untuk pengguna berlangganan.")
-    return wrapper
+        user_name = message.from_user.first_name
+        rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+        if not JOIN:  # Not compulsory
+            return
+        try:
+            try:
+                await app.get_chat_member(JOIN, message.from_user.id)
+            except UserNotParticipant:
+                if JOIN.isalpha():
+                    link = "https://t.me/" + JOIN
+                else:
+                    chat_info = await app.get_chat(JOIN)
+                    chat_info.invite_link
+                try:
+                    await message.reply(
+                        f"ʜᴇʏ {rpk}. ᴀɢᴀʀ ʙɪsᴀ ᴍᴇᴍᴜᴛᴀʀ ᴍᴜsɪᴋ, ᴀɴᴅᴀ ʜᴀʀᴜs ᴊᴏɪɴ ᴋᴇ ɢʀᴜᴘ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ . ᴋᴀʟᴀᴜ ᴀᴅᴀ ᴍᴀꜱᴀʟᴀʜ ᴘᴄ @kiritonibos",
+                        disable_web_page_preview=True,
+                        reply_markup=InlineKeyboardMarkup(
+                            [[InlineKeyboardButton("🗣️ GABUNG SINI 😅", url=link)]]
+                        ),
+                    )
+                    await message.stop_propagation()
+                except ChatWriteForbidden:
+                    pass
+        except ChatAdminRequired:
+            await message.reply(
+                f"Saya bukan admin di chat : {JOIN} !"
+            )
+        return await func(_, message)
 
-# Contoh penggunaan:
-@awdfilm
-async def your_handler(_, message: Message):
-    await message.reply("Selamat datang pengguna premium!")
+    return wrapper
