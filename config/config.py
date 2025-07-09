325a22e6878320801cc8545c60ab3809aa679ef9
iyann1255 <tessbarca1981@gmail.com>
2025-07-09 14:02:49 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,13 +1,3 @@
-# Copyright (C) 2021-2022 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-Alexa is a Telegram Audio and video streaming bot
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want.
-"""
 
 import re
 import sys
