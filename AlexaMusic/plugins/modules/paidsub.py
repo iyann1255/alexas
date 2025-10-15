c36706904d5d265f34c9be028d05a58db2be70eb
iyann1255 <tessbarca1981@gmail.com>
2025-10-15 10:09:40 +0700

    Update paidsub.py

--- AlexaMusic/plugins/modules/paidsub.py
+++ AlexaMusic/plugins/modules/paidsub.py
@@ -8,37 +8,39 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from AlexaMusic.utils.database import get_served_users, get_served_chats
 
 
-def subcribe(func):
-    async def wrapper(_, message: Message):
-        user_id = message.from_user.id
-        user_name = message.from_user.first_name
-        rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
-        if not JOIN:  # Not compulsory
-            return
-        try:
-            try:
-                await app.get_chat_member(JOIN, message.from_user.id)
-            except UserNotParticipant:
-                if JOIN.isalpha():
-                    link = "https://t.me/" + JOIN
-                else:
-                    chat_info = await app.get_chat(JOIN)
-                    chat_info.invite_link
-                try:
-                    await message.reply(
-                        f"ʜᴇʏ {rpk}. ᴀɢᴀʀ ʙɪsᴀ ᴍᴇᴍᴜᴛᴀʀ ᴍᴜsɪᴋ, ᴀɴᴅᴀ ʜᴀʀᴜs ᴊᴏɪɴ ᴋᴇ ɢʀᴜᴘ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ . ᴋᴀʟᴀᴜ ᴀᴅᴀ ᴍᴀꜱᴀʟᴀʜ ᴘᴄ @kiritonibos",
-                        disable_web_page_preview=True,
-                        reply_markup=InlineKeyboardMarkup(
-                            [[InlineKeyboardButton("🗣️ GABUNG SINI 😅", url=link)]]
-                        ),
-                    )
-                    await message.stop_propagation()
-                except ChatWriteForbidden:
-                    pass
-        except ChatAdminRequired:
-            await message.reply(
-                f"Saya bukan admin di chat : {JOIN} !"
-            )
-        return await func(_, message)
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+#--------------------------
 
-    return wrapper
+MUST_JOIN = "aseppppv"
+#------------------------
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(app: Client, msg: Message):
+    if not MUST_JOIN:
+        return
+    try:
+        try:
+            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await app.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(
+                    photo="https://files.catbox.moe/4j30vg.jpg", caption=f"<blockquote><b>Silahkan bergabung dulu [Support]({link}) ,jika ingin menggunakan bot ini, jika sudah bergabung silahkan ulangi /start</b></blockquote>",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("ðŸŽµ JOIN DULU", url="https://t.me/MaulKak"),
+                            ]
+                        ]
+                    )
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"Promosi saya sebagai admin digrup/channels yang diFSUB: {MUST_JOIN} !")
